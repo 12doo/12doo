a01bdae1b6e7434529475ba70b3ba84d5a3b370e
liuzelei <liuzelei@gmail.com>
2010-10-06 01:34:04 +0800

    add route file and gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 V3::Application.routes.draw do
+  get "store/index"
+
   resources :product_statuses
 
   resources :product_attributes
@@ -67,5 +69,5 @@ V3::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
