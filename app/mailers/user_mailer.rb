39a6c4dfaecb5c0d53b2ce90b0e3cfee33ae5e79
liuzelei <liuzelei@gmail.com>
2011-01-17 23:53:39 +0800

    translate to chinese

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "admin@12doo.com"
   
   
   def welcome_email(user)
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
-  :domain               => "12doo.com",  
-  :user_name            => "admin@12doo.com",  
+  :domain               => "12doo.net",  
+  :user_name            => "admin@12doo.net",  
   :password             => "Password01",  
   :authentication       => "plain",
   :enable_starttls_auto => true  
