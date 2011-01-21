833f597a008af58dca9fc4ae569e2d9e5945c36f
Zachary Zhang <czhang@12doo.com>
2011-01-21 09:02:33 +0800

    minor adjust

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,21 @@ ActiveRecord::Schema.define(:version => 20110120080954) do
     t.integer  "used_time",                                       :default => 0
   end
 
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
   create_table "deliveries", :force => true do |t|
     t.integer  "area_id"
     t.decimal  "door_step",  :precision => 10, :scale => 0
