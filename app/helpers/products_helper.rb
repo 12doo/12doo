6f407dbf606fc71e32c3971b6849ea648fae8beb
liuzelei <liuzelei@gmail.com>
2011-01-22 20:06:24 +0800

    search init

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,17 @@
 module ProductsHelper
+  def search_path(current_tag)
+    if params[:tags]
+      if params[:tags].split(',').include?(current_tag)
+        return "/category/#{params[:tags]}/#{params[:keywords]}"
+      else
+        return "/category/#{current_tag},#{params[:tags]}/#{params[:keywords]}"
+      end
+    else
+      return "/category/#{current_tag}/#{params[:keywords]}"
+    end
+  end
+  
+  def checked?(tag,value)
+    
+  end
 end
