e38c0c56024457aba173b4de7bfe21155c456ff7
liuzelei <liuzelei@gmail.com>
2011-01-24 14:15:26 +0800

    search tag switch

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
   
-  # 生成查询URL
+  # 生成查询URL,格式0-0-0-1,2,3-4,5-0
   def search_path(id, sort)    
     tags = []
     if params[:tags]
@@ -13,14 +13,27 @@ module ProductsHelper
 
     # 找到对应位置上是否已经有该tag
     if tags.length > sort + 1 && tags[sort] && tags[sort] != ''
-      values = tags[sort].split(',')
-      unless values.include?(id.to_s)
-        if tags[sort] == "0"
-          tags[sort] = id.to_s
+      
+      # 如果当前tag是0,清理掉所有的同节点values
+      if id == 0
+        tags[sort] = "0"
+      else
+        values = tags[sort].split(',')
+        
+        # 如果已经有该链接,从URL里删除,否则添加
+        if values.include?(id.to_s)
+          values.delete_at(values.index(id.to_s))
+          tags[sort] = values.join(',')
         else
-          tags[sort] = id.to_s + ',' + tags[sort]
+          if tags[sort] == "0"
+            tags[sort] = id.to_s
+          else
+            tags[sort] = id.to_s + ',' + tags[sort]
+          end
         end
       end
+      
+
     end
     
     "/category/#{tags.join('-')}/#{params[:keywords]}"
