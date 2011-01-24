1bd76743bcbfeeaad2f2ce1a60ff76998e9f66fd
liuzelei <liuzelei@gmail.com>
2011-01-24 16:39:49 +0800

    search url

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -12,7 +12,7 @@ module ProductsHelper
     # sort字段必须是按照0,1,2,3严格排序,不能有跳数字或者重复的情况
 
     # 找到对应位置上是否已经有该tag
-    if tags.length > sort + 1 && tags[sort] && tags[sort] != ''
+    if tags.length >= sort + 1 && tags[sort] && tags[sort] != ''
       
       # 如果当前tag是0,清理掉所有的同节点values
       if id == 0
@@ -23,6 +23,9 @@ module ProductsHelper
         # 如果已经有该链接,从URL里删除,否则添加
         if values.include?(id.to_s)
           values.delete_at(values.index(id.to_s))
+          if values.length == 0
+            values << "0"
+          end
           tags[sort] = values.join(',')
         else
           if tags[sort] == "0"
@@ -32,11 +35,28 @@ module ProductsHelper
           end
         end
       end
-      
 
     end
     
     "/category/#{tags.join('-')}/#{params[:keywords]}"
+  end
+  
+  def search_path_init(id, sort)
+    tags_count = ProductAttributeDefine.where(:search => true).count(:id)
+    tags = Array.new(tags_count, "0")
+    if tags_count >= sort
+      tags[sort] = id
+    end
+    "/category/#{tags.join('-')}/"
+  end
+  
+  def search_path_without_keywords
+    if params[:tags]
+      return "/category/#{params[:tags]}/"
+    else
+      tags_count = ProductAttributeDefine.where(:search => true).count(:id)
+      return "/category/#{Array.new(tags_count, "0").join('-')}/"
+    end
   end
   
   # 判断当前id是否选中
@@ -44,7 +64,7 @@ module ProductsHelper
     tags = []
     if params[:tags]
       tags = params[:tags].split('-')
-      if tags.length > sort + 1 && tags[sort] && tags[sort] != ''
+      if tags.length >= sort + 1 && tags[sort] && tags[sort] != ''
         values = tags[sort].split(',')
         unless values.include?(id.to_s)
           return true
