a9e7baf431b3ac3caa1ce78b293a6494777a4941
Zachary Zhang <czhang@12doo.com>
2011-05-29 17:46:56 +0800

    small change in homepage, upload 2 new products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@
 class HomeController < ApplicationController
   def index  
     
-    @promo1=Product.find(21)
-    @promo2=Product.find(22)
-    @promo3=Product.find(1)
-    @promo4=Product.find(24)
+    @promo1=Product.find(5)
+    @promo2=Product.find(1)
+    @promo3=Product.find(12)
+    @promo4=Product.find(8)
     
-    @red1=Product.find(25)
-    @red2=Product.find(26)
+    @red1=Product.find(5)
+    @red2=Product.find(8)
     @red3=Product.find(27)
     @red4=Product.find(28)
     
