af4f7da64e07319feee2321b18f2798a24b03c6d
Zachary Zhang <czhang@12doo.com>
2011-05-29 15:19:12 +0800

    update home page product select, put into controll

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,26 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
   def index  
-    params[:id] = 1
-    @product = Product.find(params[:id])
+    
+    @promo1=Product.find(21)
+    @promo2=Product.find(22)
+    @promo3=Product.find(1)
+    @promo4=Product.find(24)
+    
+    @red1=Product.find(25)
+    @red2=Product.find(26)
+    @red3=Product.find(27)
+    @red4=Product.find(28)
+    
+    @white1=Product.find(29)
+    @white2=Product.find(1)
+    @white3=Product.find(11)
+    @white4=Product.find(12)
+    
+    @other1=Product.find(1)
+    @other2=Product.find(10)
+    @other3=Product.find(13)
+    @other4=Product.find(15)
+   
   end
 end
