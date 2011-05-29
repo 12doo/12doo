39d7da775419b3e6fababbc910c86738960fab16
Zachary Zhang <czhang@12doo.com>
2011-05-29 20:03:50 +0800

    add some new products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,14 @@
 class HomeController < ApplicationController
   def index  
     
-    @promo1=Product.find(5)
+    @promo1=Product.find(10)
     @promo2=Product.find(1)
-    @promo3=Product.find(12)
-    @promo4=Product.find(8)
+    @promo3=Product.find(9)
+    @promo4=Product.find(5)
     
     @red1=Product.find(5)
     @red2=Product.find(8)
-    @red3=Product.find(27)
+    @red3=Product.find(10)
     @red4=Product.find(28)
     
     @white1=Product.find(29)
@@ -17,9 +17,9 @@ class HomeController < ApplicationController
     @white3=Product.find(11)
     @white4=Product.find(12)
     
-    @other1=Product.find(1)
-    @other2=Product.find(10)
-    @other3=Product.find(13)
+    @other1=Product.find(9)
+    @other2=Product.find(1)
+    @other3=Product.find(15)
     @other4=Product.find(15)
    
   end
