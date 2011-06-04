0e8c904b36919e485393d5b2351f6980c4ce22f9
Zachary Zhang <czhang@12doo.com>
2011-06-04 22:55:37 +0800

    update password reset feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,15 @@ V3::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => '12doo.net',
+    :user_name            => 'admin@12doo.net',
+    :password             => 'Password01',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
   
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
