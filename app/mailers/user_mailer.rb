002b8f80c8010e4257fa266046687093fa9462ae
Zachary Zhang <czhang@12doo.com>
2011-06-06 23:58:41 +0800

    revise mailer format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 # -*- encoding : utf-8 -*-
 class UserMailer < ActionMailer::Base
-  default :from => "12doo红酒网 <admin@12doo.com>"
+  default :from => "admin@12doo.com"
  
   def welcome_email(user)
     @user = user
-    mail(:to => "#{user.display_name} <#{user.email}>",
+    mail(:to => @user.email,
          :subject => "欢迎来到12度红酒网")
   end
   
