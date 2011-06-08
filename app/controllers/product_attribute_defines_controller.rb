f11edafdffc6dc1acb2346369b600ff23f38f4c6
liuzelei <liuzelei@gmail.com>
2011-06-08 19:50:13 +0800

    product edit update

--- app/controllers/product_attribute_defines_controller.rb
+++ app/controllers/product_attribute_defines_controller.rb
@@ -2,46 +2,23 @@
 class ProductAttributeDefinesController < ApplicationController
   # 身份验证
   before_filter :authorize_admin!
-  # GET /product_attribute_defines
-  # GET /product_attribute_defines.xml
+
   def index
     @product_attribute_defines = ProductAttributeDefine.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @product_attribute_defines }
-    end
   end
 
-  # GET /product_attribute_defines/1
-  # GET /product_attribute_defines/1.xml
   def show
     @product_attribute_define = ProductAttributeDefine.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @product_attribute_define }
-    end
   end
 
-  # GET /product_attribute_defines/new
-  # GET /product_attribute_defines/new.xml
   def new
     @product_attribute_define = ProductAttributeDefine.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @product_attribute_define }
-    end
   end
 
-  # GET /product_attribute_defines/1/edit
   def edit
     @product_attribute_define = ProductAttributeDefine.find(params[:id])
   end
 
-  # POST /product_attribute_defines
-  # POST /product_attribute_defines.xml
   def create
     @product_attribute_define = ProductAttributeDefine.new(params[:product_attribute_define])
 
