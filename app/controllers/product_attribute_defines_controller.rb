d058d09dfcc3b9dd62b355b66354fe4a789a79d1
liuzelei <liuzelei@gmail.com>
2011-06-08 23:27:37 +0800

    edit product with attribute

--- app/controllers/product_attribute_defines_controller.rb
+++ app/controllers/product_attribute_defines_controller.rb
@@ -24,33 +24,27 @@ class ProductAttributeDefinesController < ApplicationController
 
     respond_to do |format|
       if @product_attribute_define.save
-        format.html { redirect_to(@product_attribute_define, :notice => 'Product attribute define was successfully created.') }
-        format.xml  { render :xml => @product_attribute_define, :status => :created, :location => @product_attribute_define }
+        @product_attribute_define.save_values params[:values]
+        format.html { redirect_to :action => :index }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @product_attribute_define.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /product_attribute_defines/1
-  # PUT /product_attribute_defines/1.xml
   def update
     @product_attribute_define = ProductAttributeDefine.find(params[:id])
 
     respond_to do |format|
       if @product_attribute_define.update_attributes(params[:product_attribute_define])
-        format.html { redirect_to(@product_attribute_define, :notice => 'Product attribute define was successfully updated.') }
-        format.xml  { head :ok }
+        @product_attribute_define.save_values params[:values]
+        format.html { redirect_to :action => :index }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @product_attribute_define.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /product_attribute_defines/1
-  # DELETE /product_attribute_defines/1.xml
   def destroy
     @product_attribute_define = ProductAttributeDefine.find(params[:id])
     @product_attribute_define.destroy
