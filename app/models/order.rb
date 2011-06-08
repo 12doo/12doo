eeb260238d069cebd3b0f44636252d891b92060b
Zachary Zhang <czhang@12doo.com>
2011-06-09 00:16:02 +0800

    new order triggle mail to user

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,15 @@
 # -*- encoding : utf-8 -*-
 class Order < ActiveRecord::Base
-    has_many :order_items
-    belongs_to :user
-    paginates_per 10
+  
+  after_create :send_order_confirm_email
+  
+  has_many :order_items
+  belongs_to :user
+  paginates_per 10
+    
+  def send_order_confirm_email
+    Thread.start do 
+      OrderMailer.order_confirm_email(self).deliver
+    end
+  end
 end
