aeb3ba819dae3e394f1be62796e2bfb155d660f7
liuzelei <liuzelei@gmail.com>
2011-06-10 11:54:46 +0800

    uncheck all attribute

--- app/helpers/product_attribute_defines_helper.rb
+++ app/helpers/product_attribute_defines_helper.rb
@@ -1,3 +1,20 @@
 # -*- encoding : utf-8 -*-
 module ProductAttributeDefinesHelper
+  def get_attribute_define_id_by_short(short)
+    item = ProductAttributeDefine.find_by_short(short)
+    if item
+      return item.id
+    else
+      return 0
+    end
+  end
+  
+  def get_attribute_define_sort_by_short(short)
+    item = ProductAttributeDefine.find_by_short(short)
+    if item
+      return item.sort
+    else
+      return 0
+    end
+  end
 end
