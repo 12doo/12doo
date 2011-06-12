1b94411302cae0d682542e239097695d442d0aaf
root <root@vps.12doo.net>
2011-06-13 00:09:10 +0800

    local save

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,24 +2,24 @@
 class HomeController < ApplicationController
   def index  
     
-    @promo1=Product.find(10)
-    @promo2=Product.find(1)
-    @promo3=Product.find(9)
-    @promo4=Product.find(5)
+    @promo1=Product.find(14)
+    @promo2=Product.find(11)
+    @promo3=Product.find(3)
+    @promo4=Product.find(1)
     
-    @red1=Product.find(5)
-    @red2=Product.find(8)
-    @red3=Product.find(10)
-    @red4=Product.find(1)
+    @red1=Product.find(2)
+    @red2=Product.find(7)
+    @red3=Product.find(4)
+    @red4=Product.find(5)
     
     @white1=Product.find(1)
-    @white2=Product.find(1)
-    @white3=Product.find(8)
-    @white4=Product.find(9)
+    @white2=Product.find(3)
+    @white3=Product.find(3)
+    @white4=Product.find(1)
     
-    @other1=Product.find(9)
-    @other2=Product.find(1)
-    @other3=Product.find(10)
+    @other1=Product.find(11)
+    @other2=Product.find(14)
+    @other3=Product.find(7)
     @other4=Product.find(5)
    
   end
