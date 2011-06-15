d55ff1873272b10267175f1c0ad17aeda75ef009
Zachary Zhang <czhang@12doo.com>
2011-06-16 00:45:29 +0800

    fix wrong migrate file

--- db/migrate/20110614074924_rename_pictures_column_name.rb
+++
@@ -1,9 +0,0 @@
-class RenamePicturesColumnName < ActiveRecord::Migration
-  def self.up
-    rename_column :pictures, :item, :item_file_name
-  end
-
-  def self.down
-    rename_column :pictures, :item_file_name, :item
-  end
-end
