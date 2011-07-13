88c7234b4f4fd19a2afd21e3b394c2239843fb1f
liuzelei <liuzelei@gmail.com>
2011-07-13 22:59:49 +0800

    miss migration

---
+++ db/migrate/20110713142814_update_all_money_columns.rb
@@ -0,0 +1,31 @@
+class UpdateAllMoneyColumns < ActiveRecord::Migration
+  def self.up
+    change_column :alipay_logs, :price, :decimal, :precision => 10, :scale => 2
+    change_column :alipay_logs, :total_fee, :decimal, :precision => 10, :scale => 2
+    change_column :coupons, :discount, :decimal, :precision => 10, :scale => 2
+    change_column :coupons, :threshold, :decimal, :precision => 10, :scale => 2
+    change_column :deliveries, :door_step, :decimal, :precision => 10, :scale => 2
+    change_column :dispatch_items, :product_price, :decimal, :precision => 10, :scale => 2
+    change_column :dispatch_items, :subtotal, :decimal, :precision => 10, :scale => 2
+    change_column :dispatches, :order_total, :decimal, :precision => 10, :scale => 2
+    change_column :dispatches, :order_carriage, :decimal, :precision => 10, :scale => 2
+    change_column :dispatches, :order_pay_price, :decimal, :precision => 10, :scale => 2
+    change_column :dispatches, :order_discount, :decimal, :precision => 10, :scale => 2
+    change_column :favorites, :price, :decimal, :precision => 10, :scale => 2
+  end
+
+  def self.down
+    change_column :alipay_logs, :price, :decimal, :precision => 10, :scale => 0
+    change_column :alipay_logs, :total_fee, :decimal, :precision => 10, :scale => 0
+    change_column :coupons, :discount, :decimal, :precision => 10, :scale => 0
+    change_column :coupons, :threshold, :decimal, :precision => 10, :scale => 0
+    change_column :deliveries, :door_step, :decimal, :precision => 10, :scale => 0
+    change_column :dispatch_items, :product_price, :decimal, :precision => 10, :scale => 0
+    change_column :dispatch_items, :subtotal, :decimal, :precision => 10, :scale => 0
+    change_column :dispatches, :order_total, :decimal, :precision => 10, :scale => 0
+    change_column :dispatches, :order_carriage, :decimal, :precision => 10, :scale => 0
+    change_column :dispatches, :order_pay_price, :decimal, :precision => 10, :scale => 0
+    change_column :dispatches, :order_discount, :decimal, :precision => 10, :scale => 0
+    change_column :favorites, :price, :decimal, :precision => 10, :scale => 0
+  end
+end
