539af8d4e9afcaa2ec46aafcb056ad1dd2cee1a0
liuzelei <liuzelei@gmail.com>
2011-07-16 17:41:38 +0800

    temp commit for fix bug

---
+++ db/migrate/20110716085700_add_column_category_id_to_products.rb
@@ -0,0 +1,11 @@
+class AddColumnCategoryIdToProducts < ActiveRecord::Migration
+  def self.up
+    add_column :products, :category_id, :integer
+    add_column :products, :template_name, :string, :default => ''
+  end
+
+  def self.down
+    remove_column :products, :category_id
+    remove_column :products, :template_name
+  end
+end
