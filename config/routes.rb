e651952e6252b7b3f6d81c1655b481f915afd894
liuzelei <liuzelei@gmail.com>
2011-07-16 17:39:31 +0800

    temp commit for fix master bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 V3::Application.routes.draw do
 
+  resources :categories
+
   resources :coupons, :deliveries, :addresses, :order_items, :order_changes, :pictures, :alipay_logs, :dispatches, :dispatch_items,
             :product_tags, :product_statuses, :product_attributes, :product_attribute_defines, :products, :promotions
             
