32745ca6d406336d3af50ebe4b2b2c55a1d80bd7
liuzelei <liuzelei@gmail.com>
2011-07-22 14:15:29 +0800

    fix query bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,20 +23,19 @@ class ProductsController < ApplicationController
       sort_by = params[:sort_by]
     end
     
-    if (params[:tags] == nil) && (params[:keywords] == nil)
+    if (params[:tags] == nil || condition_is_null) && (params[:keywords] == nil || params[:keywords] == '')
       @products = Product.where(:visiable => true).order('id desc').page(params[:page]).per(9)
     else
       sku = []
-      
       if (params[:tags] == nil || condition_is_null) && params[:keywords] && params[:keywords] != ''
         # 如果tag为空,keywords不为空
         sku = ProductAttribute.select("distinct(product_sku)").where("value like :keywords", :keywords => "%#{params[:keywords]}%")
-      elsif params[:keywords] == nil && params[:tags] && params[:tags] != ''
+      elsif params[:keywords] == nil && params[:tags] && params[:tags] != '' && !condition_is_null
         # 如果keywords为空,tags不为空
-        sku = ProductAttribute.select("product_sku").where("product_attribute_value_id in (:skus)", :skus => params[:tags].split(/,|-/)).group("product_sku").having("count(*) > 1")
+        sku = ProductAttribute.select("product_sku").where("product_attribute_value_id in (:skus)", :skus => params[:tags].split(/,|-/).delete_if{|item| item == "0"}).group("product_sku").having("count(*) > #{effective_section_count}")
       else
         # 如果都不为空
-        sku = ProductAttribute.select("product_sku").where("(product_attribute_value_id in (:skus)) or value like :keywords",:skus => params[:tags].split(/,|-/), :keywords => "%#{params[:keywords]}%").group("product_sku").having("count(*) > 1")
+        sku = ProductAttribute.select("product_sku").where("(product_attribute_value_id in (:skus)) or value like :keywords",:skus => params[:tags].split(/,|-/).delete_if{|item| item == "0"}, :keywords => "%#{params[:keywords]}%").group("product_sku").having("count(*) > #{effective_section_count}")
       end
       
       skus = []
@@ -250,6 +249,16 @@ class ProductsController < ApplicationController
       end
     end
     return true
+  end
+  
+  def effective_section_count
+    flag = -1
+    params[:tags].split('-').each do |item|
+      if item && item != '' && item != "0"
+          flag = flag + 1
+      end
+    end
+    flag
   end
   
 end
