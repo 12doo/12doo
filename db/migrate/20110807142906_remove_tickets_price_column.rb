9503af3853c632cbfa0583d2bc692ab419ca3a8c
liuzelei <liuzelei@gmail.com>
2011-08-07 23:07:31 +0800

    remove price column from ticket

---
+++ db/migrate/20110807142906_remove_tickets_price_column.rb
@@ -0,0 +1,9 @@
+class RemoveTicketsPriceColumn < ActiveRecord::Migration
+  def self.up
+    remove_column :tickets, :price
+  end
+
+  def self.down
+    add_column :tickets, :price, :precision => 10, :scale => 2, :default => 0
+  end
+end
