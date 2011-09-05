dc3e2762cdfab27cc89346906fad493888be0694
liuzelei <liuzelei@gmail.com>
2011-09-05 14:43:05 +0800

    remove order_item sku relationship

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,11 +2,8 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :user
+  belongs_to :product
   
   cattr_reader :per_page
   paginates_per 20
-  
-  def product
-    Product.find_by_sku(product_sku)
-  end
 end
