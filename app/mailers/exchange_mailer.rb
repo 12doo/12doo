97d296edc10324cf358b96efe6f54bbbe192cc71
liuzelei <liuzelei@gmail.com>
2011-09-05 14:07:07 +0800

    exchange mail

--- app/mailers/exchange_mailer.rb
+++ app/mailers/exchange_mailer.rb
@@ -3,7 +3,7 @@ class ExchangeMailer < ActionMailer::Base
   default :from => "admin@12doo.com"
  
   def exchange_inform_email(exchange)
-    @exchange = order
+    @exchange = exchange
     mail(:to => 'admin@12doo.com',
          :subject => "收到新提货预约" + exchange.no)
   end
