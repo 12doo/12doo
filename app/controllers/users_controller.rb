4f87f0f261b33281bd453ac003b0ae92bb007a12
liuzelei <liuzelei@gmail.com>
2011-09-13 00:43:37 +0800

    show users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 # -*- encoding : utf-8 -*-
 class UsersController < ApplicationController
+  # 身份验证
+  before_filter :authorize_admin!, :except => [:welcome]
+  
   def index
     @users = User.order("id desc").page(params[:page])
   end
   
   def show
+    @user = User.find(params[:id])
   end
   
   def set_role
