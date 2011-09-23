e6952a035eec1c4aa52360bcb50921ae55413efa
liuzelei <liuzelei@gmail.com>
2011-09-23 16:49:46 +0800

    cat path

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -47,31 +47,31 @@ class SearchController < ApplicationController
       if (params[:tags] == nil || condition_is_null) && (params[:keywords] == nil || params[:keywords] == '')
         @products = Product.where(:visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
       else
-        sku = []
+        id = []
         if (params[:tags] == nil || condition_is_null) && params[:keywords] && params[:keywords] != ''
           # 如果tag为空,keywords不为空
-          sku = ProductAttribute.select("distinct(product_sku)").where("value like :keywords", :keywords => "%#{params[:keywords]}%")
+          id = ProductAttribute.select("distinct(product_id)").where("value like :keywords", :keywords => "%#{params[:keywords]}%")
         elsif params[:keywords] == nil && params[:tags] && params[:tags] != '' && !condition_is_null
           # 如果keywords为空,tags不为空
-          sku = ProductAttribute.select("product_sku").where("product_attribute_value_id in (:skus)", :skus => params[:tags].split(/,|-/).delete_if{|item| item == "0"}).group("product_sku").having("count(*) > #{effective_section_count}")
+          id = ProductAttribute.select("product_id").where("product_attribute_value_id in (:ids)", :ids => params[:tags].split(/,|-/).delete_if{|item| item == "0"}).group("product_id").having("count(*) > #{effective_section_count}")
         else
           # 如果都不为空
-          sku = ProductAttribute.select("product_sku").where("(product_attribute_value_id in (:skus)) or value like :keywords",:skus => params[:tags].split(/,|-/).delete_if{|item| item == "0"}, :keywords => "%#{params[:keywords]}%").group("product_sku").having("count(*) > #{effective_section_count}")
+          id = ProductAttribute.select("product_id").where("(product_attribute_value_id in (:ids)) or value like :keywords",:ids => params[:tags].split(/,|-/).delete_if{|item| item == "0"}, :keywords => "%#{params[:keywords]}%").group("product_id").having("count(*) > #{effective_section_count}")
         end
       
-        skus = []
-        sku.each do |item|
-          skus << item.product_sku
+        ids = []
+        id.each do |item|
+          ids << item.product_id
         end
       
         if params[:keywords] && params[:keywords] != ''
           if condition_is_null
-            @products = Product.where("(sku in (:skus) or cn_name like :cn_name or name like :name) and visiable = :visiable", :skus => skus, :cn_name => "%#{params[:keywords]}%", :name => "%#{params[:keywords]}%", :visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
+            @products = Product.where("(id in (:ids) or cn_name like :cn_name or name like :name) and visiable = :visiable", :ids => ids, :cn_name => "%#{params[:keywords]}%", :name => "%#{params[:keywords]}%", :visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
           else
-            @products = Product.where("(sku in (:skus) and (cn_name like :cn_name or name like :name)) and visiable = :visiable", :skus => skus, :cn_name => "%#{params[:keywords]}%", :name => "%#{params[:keywords]}%", :visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
+            @products = Product.where("(id in (:ids) and (cn_name like :cn_name or name like :name)) and visiable = :visiable", :ids => ids, :cn_name => "%#{params[:keywords]}%", :name => "%#{params[:keywords]}%", :visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
           end
         else
-          @products = Product.where("sku in (:skus) and visiable = :visiable", :skus => skus, :visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
+          @products = Product.where("id in (:ids) and visiable = :visiable", :ids => ids, :visiable => true).order("#{sort_by} #{sort}").page(params[:page]).per(12)
         end
       end
     else
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,3 +1,7 @@
 # -*- encoding : utf-8 -*-
 module CategoriesHelper
+  def cat_path(*cats)
+    tags_count = ProductAttributeDefine.where("category_id in (:ids) and search = :search", :ids => cats, :search => true).count(:id)
+    "/category/#{cats.join('-')}/#{Array.new(tags_count, "0").join('-')}/"
+  end
 end
