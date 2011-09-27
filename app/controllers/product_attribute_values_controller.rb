370c19c056d988ffe7fd04d821292ce170199dcb
liuzelei <liuzelei@gmail.com>
2011-09-28 00:54:45 +0800

    edit and add value

--- app/controllers/product_attribute_values_controller.rb
+++ app/controllers/product_attribute_values_controller.rb
@@ -15,7 +15,6 @@ class ProductAttributeValuesController < ApplicationController
     @product_attribute_define = ProductAttributeDefine.find(params[:define_id])
 
     @product_attribute_value = ProductAttributeValue.new(params[:product_attribute_value])
-    @product_attribute_value.short = @product_attribute_define.short
     @product_attribute_value.name = @product_attribute_define.name
     @product_attribute_value.category_id = @product_attribute_define.category_id
     @product_attribute_value.product_attribute_define_id = @product_attribute_define.id
